50ff88421681676647ccc0245a3f8cee29df7fe6
yukaliv <yuka.okamoto0810@gmail.com>
2019-04-17 22:39:19 -0700

    added slide in and out feature to the carousel

--- Carousel/carousel.js
+++ Carousel/carousel.js
@@ -5,24 +5,35 @@ class Carousel {
         this.currentIndex = 0;
         this.paragraphs[this.currentIndex].style.display = 'block';
         console.log('hi');
-
-
     }
 
     moveLeft() {
-        console.log('hello');
-        this.paragraphs[this.currentIndex].style.display = 'none';
-        this.currentIndex -= 1;
-        if (this.currentIndex == -1) {
-            this.currentIndex = 2;
-        }
-        console.log("current index", this.currentIndex);
-        this.paragraphs[this.currentIndex].style.display = 'block';
+        let currentBrowserWidth = $(window).width();
+        let centerPoint = Math.floor(currentBrowserWidth / 2);
+        let currentIndex = this.currentIndex;
+        let self = this;
+        let currentParagraph = this.paragraphs[this.currentIndex];
 
+        $(currentParagraph).css("margin-left", currentBrowserWidth * 2 + "px");
+        currentParagraph.style.display = 'block';
+        $(currentParagraph).animate({ marginLeft: "5%" }, 1000,
+            function () {
+                function sleep(time) {
+                    return new Promise((resolve) => setTimeout(resolve, time));
+                }
+                sleep(2000).then(function () {
+                    $(currentParagraph).animate({ marginLeft: currentBrowserWidth * -2 }, 1000);
+                });
+                currentIndex -= 1;
+                if (currentIndex == -1) {
+                    currentIndex = 2;
+                }
+                self.currentIndex = currentIndex;
+            });
     }
 
 }
 
 let carousel = Array.from(document.querySelectorAll('.carousel')).map(carousel => new Carousel(carousel));
 
-setInterval(function(){carousel[0].moveLeft()}, 3000);
+setInterval(function () { carousel[0].moveLeft() }, 4500);
